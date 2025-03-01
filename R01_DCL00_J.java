8cf8f893870a2dddc6629949f95546bdbbec62e1
matteodagostino <ma950740@ucf.edu>
2025-03-01 18:47:55 -0500

    Update R01_DCL00_J.java

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -1,13 +1,10 @@
 
-public class Cycle {
-  private final int balance;
-  private static final int deposit = (int) (Math.random() * 100); // Random deposit
-  private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
-  public Cycle() {
-    balance = deposit - 10; // Subtract processing fee
+public class Widget {
+  private int total; // Declared private
+ 
+  public int getTotal () {
+    return total;
   }
  
-  public static void main(String[] args) {
-    System.out.println("The account balance is: " + c.balance);
-  }
+  // Definitions for add() and remove() remain the same
 }
