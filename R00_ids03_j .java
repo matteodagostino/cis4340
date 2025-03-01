ff4d4548c07c49253d11c43ab5a475225011e30d
matteodagostino <ma950740@ucf.edu>
2025-03-01 17:17:55 -0500

    Update R00_ids03_j .java
    corrected the code

--- R00_ids03_j .java
+++ R00_ids03_j .java
@@ -1,15 +1,9 @@
-if( loginSuccessful) {
-logger . s e v e r e (” User l o g i n s u c c e e d e d f o r : ” + username ) ;
-} e l s e {
-l o g g e r . s e v e r e (” User l o g i n f a i l e d f o r : ” + username ) ;
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
 }
 Correct the code as shown in theCompliant Solution below:
-i f ( l o g i n S u c c e s s f u l ) {
-l o g g e r . s e v e r e (” User l o g i n s u c c e e d e d f o r : ” + s a n i t i z e U s e r ( username ) ) ;
-} e l s e {
-l o g g e r . s e v e r e (” User l o g i n f a i l e d f o r : ” + s a n i t i z e U s e r ( username ) ) ;
-}
-p u bli c S t ri n g s a n i t i z e U s e r ( S t ri n g username ) {
-r e t u r n Pa t te rn . matches ( ” [A−Za−z0−9 ]+” , username ) )
-? username : ” u na u t ho ri z e d u s e r ” ;
-}
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
