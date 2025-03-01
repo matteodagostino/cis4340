08a8492152643ac2ffa7918628a453b618cf5a1a
matteodagostino <ma950740@ucf.edu>
2025-03-01 17:26:01 -0500

    Update RO3_NUM03_J.java

--- RO3_NUM03_J.java
+++ RO3_NUM03_J.java
@@ -1,3 +1,3 @@
-p u bli c s t a t i c i n t g e t I n t e g e r ( DataInputStream i s ) throws IOException {
-r e t u r n i s . r e a d I n t ( ) ;
+public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
