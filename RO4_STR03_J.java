8c7c2f200b91dda81bdb02d831aed462ebc8e6f4
matteodagostino <ma950740@ucf.edu>
2025-03-01 18:32:19 -0500

    Update RO4_STR03_J.java

--- RO4_STR03_J.java
+++ RO4_STR03_J.java
@@ -1,5 +1,5 @@
-Bi g I n t e g e r x = new Bi g I n t e g e r (”530500452766” );
-byte [ ] byteArray = x . toByteArray ( ) ;
-S t ri n g s = new S t ri n g ( byteArray ) ;
-byteArray = s . g e tB y t e s ( ) ;
-x = new Bi g I n t e g e r ( byteArray ) ;
+BigInteger x = new BigInteger("530500452766");
+String s = x.toString();  // Valid character data
+byte[] byteArray = s.getBytes();
+String ns = new String(byteArray); 
+x = new BigInteger(ns);
